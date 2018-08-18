f0ba6762f82230eadeba139b7c826d610b72ed5b
Raul Candido <raul.cand@hotmail.com>
2018-08-18 20:08:59 -0300

    inicio da classe bola

---
+++ js/obj/bola.js
@@ -0,0 +1,19 @@
+function Bola(context){
+    this.context = context;
+    this.x = 0;
+    this.y = 0;
+    this.velocidadeX = 0;
+    this.velocidadeY = 0;
+
+    this.cor = "black";
+    this.raio = "10";
+}
+Bola.prototype = {
+    atualizar: function(){
+
+    },
+    desenhar: function(){
+
+    }
+    
+}
