768dbb5bafb104a7f3c13fb60310fad3b2f3a10c
Raul Candido <raul.cand@hotmail.com>
2018-08-19 21:12:29 -0300

    finalização da implementação de colisão.

--- js/obj/bola.js
+++ js/obj/bola.js
@@ -11,12 +11,12 @@ function Bola(context) {
 Bola.prototype = {
     atualizar: function () {
         var context = this.context;
-        // if (this.x < this.raio || this.x > context.canvas.width - this.raio) {
-        //     this.velocidadeX *= -1;
-        // }
-        // if (this.y < this.raio || this.y > context.canvas.height - this.raio) {
-        //     this.velocidadeY *= -1;
-        // }
+        if (this.x < this.raio || this.x > context.canvas.width - this.raio) {
+            this.velocidadeX *= -1;
+        }
+        if (this.y < this.raio || this.y > context.canvas.height - this.raio) {
+            this.velocidadeY *= -1;
+        }
         this.x += this.velocidadeX;
         this.y += this.velocidadeY;
     },
@@ -27,9 +27,28 @@ Bola.prototype = {
         context.fillStyle = this.cor;
 
         context.beginPath();
-        context.arc(this.x,this.y,this.raio,0,2*Math.PI,false);
+        context.arc(this.x, this.y, this.raio, 0, 2 * Math.PI, false);
         context.fill();
         context.restore();
-    }
+    },
+    retangulosColisao: function () {
+        return [{
+            x: this.x - this.raio, // centro da bola
+            y: this.y - this.raio, // idem
+            largura: this.raio * 2,
+            altura: this.raio * 2
+        }];
+    },
+    colidiuCom: function (sprite) {
+        if (this.x < sprite.x) {
+            this.velocidadeX = -Math.abs(this.velocidadeX);
+        } else {
+            this.velocidadeX = Math.abs(this.velocidadeX);
+        }
+        if (this.y < sprite.y) {
+            this.velocidadeY = -Math.abs(this.velocidadeY);
+        }
+    },
+
 
 }
---
+++ js/obj/colisor.js
@@ -0,0 +1,73 @@
+function Colisor() {
+    this.sprites = [];
+    this.aoColidir = null;
+}
+Colisor.prototype = {
+    novoSprite: function (sprite) {
+        this.sprites.push(sprite);
+    },
+    processar: function () {
+
+        var jaTestados = new Object();
+
+        for (var i in this.sprites) {
+            for (var j in this.sprites) {
+                if (i == j) {
+                    continue;
+                }
+                // this.testarColisao(this.sprites[i], this.sprites[j]);
+                var id1 = this.stringUnica(this.sprites[i]);
+                var id2 = this.stringUnica(this.sprites[j]);
+
+                if (!jaTestados[id1]) {
+                    jaTestados[id1] = [];
+                }
+                if (!jaTestados[id2]) {
+                    jaTestados[id2] = [];
+                }
+                if (!(jaTestados[id1].indexOf(id2) >= 0 || jaTestados[id2].indexOf(id1) >= 0)) {
+                    this.testarColisao(this.sprites[i], this.sprites[j]);
+                    jaTestados[id1].push(id2);
+                    jaTestados[id2].push(id1);
+                }
+            }
+        }
+    },
+    testarColisao: function (sprite1, sprite2) {
+        var rets1 = sprite1.retangulosColisao();
+        var rets2 = sprite2.retangulosColisao();
+        colisoes:
+            for (var i in rets1) {
+                for (var j in rets2) {
+                    if (this.retangulosColidem(rets1[i], rets2[j])) {
+                        sprite1.colidiuCom(sprite2);
+                        sprite2.colidiuCom(sprite1);
+                        if (this.aoColidir) {
+                            this.aoColidir(sprite1, sprite2);
+                        }
+                        break colisoes;
+                    }
+                }
+            }
+    },
+    retangulosColidem: function (ret1, ret2) {
+        var colide = (ret1.x + ret1.largura) > ret2.x &&
+            ret1.x < (ret2.x + ret2.largura) &&
+            (ret1.y + ret1.largura) > ret2.y &&
+            ret1.y < (ret2.y + ret2.altura);
+        return colide;
+    },
+    stringUnica: function (sprite) {
+        var str = "";
+        var retangulos = sprite.retangulosColisao();
+
+        for (var i in retangulos) {
+            str += "x:" + retangulos[i].x + "," +
+                "y:" + retangulos[i].y + "," +
+                "l:" + retangulos[i].largura + "," +
+                "a:" + retangulos[i].altura + "\n";
+        }
+        return str;
+    },
+
+}
